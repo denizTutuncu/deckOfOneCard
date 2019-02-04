570116aab649b4387615275b33ee2d53a5474923
Deniz Tutuncu <deniztutuncu@Denizs-MacBook-Pro.local>
2019-02-04 14:25:46 -0700

    3rd commit

--- Deck_Of_One_Card/Controllers/Model Controller/CardController.swift
+++ Deck_Of_One_Card/Controllers/Model Controller/CardController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Deniz Tutuncu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class CardController {
     //CRUD
@@ -25,26 +25,50 @@ class CardController {
         //Build my request
         guard let requestURL = components?.url else { return }
         print(requestURL)
-            // Get my data
+        // Get my data
         var request = URLRequest(url: requestURL)
         request.httpMethod = "GET"
         
         let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
             do {
-            if let downloadError = error { throw downloadError }
-            guard let data = data else { throw NSError() }
-            
-            //Decode
-            let jsonDecoder = JSONDecoder()
-            let deck = try! jsonDecoder.decode(TopLevelDictionary.self, from: data)
-            completion(deck.cards)
-            
-        } catch {
-            print("Error \(requestURL)")
-            completion([])
-            return
+                if let downloadError = error { throw downloadError }
+                guard let data = data else { throw NSError() }
+                
+                //Decode
+                let jsonDecoder = JSONDecoder()
+                let deck = try! jsonDecoder.decode(TopLevelDictionary.self, from: data)
+                completion(deck.cards)
+                
+            } catch {
+                print("Error \(requestURL)")
+                completion([])
+                return
+            }
         }
-    }
         dataTask.resume()
-}
+    }
+    
+    static func image(forURL urlString: String, completion: @escaping (UIImage?) -> Void) {
+        //create the URL
+        guard let url = URL(string: urlString) else { return print("Error trying to create your umage URL")}
+        
+        //dataTask
+        let imageDataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            //Handle the Error
+            if let imageError = error {
+                print("Error retriving image \(imageError.localizedDescription)")
+                completion(nil)
+            }
+            guard let data = data, let image = UIImage(data: data) else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+        imageDataTask.resume()
+    }
 }
--- Deck_Of_One_Card/Controllers/View Controller/CardViewController.swift
+++ Deck_Of_One_Card/Controllers/View Controller/CardViewController.swift
@@ -9,9 +9,27 @@
 import UIKit
 
 class CardViewController: UIViewController {
-
+    
+    @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var drawCardButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+    }
+    
+    
+    func drawCard() {
+        CardController.drawCard(numberOfCard: 1) { (cards) in
+            let card = cards[0]
+            CardController.image(forURL: card.image, completion: { (cardImage) in
+                guard let image = cardImage else { return }
+                self.cardImageView.image = image
+            })
+        }
+    }
+    
+    @IBAction func drawCardButtonTapped(_ sender: UIButton) {
+        drawCard()
     }
 }
